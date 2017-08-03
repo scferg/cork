1263acf235ca529d801969ac42be76aff4b3553c
Sean Ferguson <scferg11@gmail.com>
2017-08-03 12:03:31 -0400

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cork
+
+Inspired by [FastShell](https://hosseinkarami.github.io/fastshell) with some modifications.
---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * cork
+ * Front-end boilerplate based on FastShell
+ * https://github.io/scferg
+ * @author Sean Ferguson
+ * @version 1.0.0
+ * Copyright 2017. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+})(jQuery, window, document);

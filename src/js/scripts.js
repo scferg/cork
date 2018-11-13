a6a164a716a4be41c167c2c305c57a18f3b8e569
Sean Ferguson <scferg11@gmail.com>
2018-11-12 20:15:42 -0500

    Revert "Update"
    This reverts commit e6d70cde687ab37dc6b6cc2686cba9e29a575a1b.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,21 +3,7 @@
   'use strict';
 
   $(function () {
-    /**
-     * Transform input type for better UX
-     */
-    $('[data-type]').each(function() {
-      $(this).prop('type', $(this).data('type'));
-    });
-
-    /**
-     * Prevent new lines in textarea to prevent data issues in MF
-     */
-    $('textarea').keypress(function(e) {
-      if (e.keyCode === 13) {
-        e.preventDefault();
-      }
-    });
+    // FastShell
   });
 
 })(jQuery, window, document);
